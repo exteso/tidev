3497159049cfcf74a9a852ef297422b579c2f45c
celestino.bellone <celestino.bellone@gmail.com>
2014-08-15 11:20:08 +0200

    add README, improve responsiveness

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -3,6 +3,17 @@
 var SiteUtils = SiteUtils || {};
 
 (function () {
+    
+    $('window').load(function() {
+        new FastClick(document.body);
+    });
+    
+    $('document').ready(function() {
+        $('a,input,:button').each(function(n, e) {
+            FastClick.attach(e);
+        });
+    });
+        
     SiteUtils.switchLanguage = function(baseCtx, newCtx) {
         var currentAddress = window.location.pathname;
         var actualAddress = currentAddress;
@@ -11,5 +22,5 @@ var SiteUtils = SiteUtils || {};
         }
         window.location.pathname=newCtx + actualAddress;
         return false;
-    }
+    };    
 })();
